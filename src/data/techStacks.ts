066fb0d0d397a44a79ca6def05051e52334f5807
Luckytime1996 <larb26656@gmail.com>
2025-08-16 22:02:13 +0700

    fix: responsive in navigationbar

--- src/data/techStacks.ts
+++ src/data/techStacks.ts
@@ -10,37 +10,44 @@ export const techStacks: TechStackCategory[] = [
   {
     title: "Languages",
     items: [
-      { img: "", name: "Java" },
-      { img: "", name: "JavaScript" },
-      { img: "", name: "TypeScript" },
-      { img: "", name: "Go" },
-      { img: "", name: "Dart" },
+      { img: "/images/techstack/java.svg", name: "Java" },
+      { img: "/images/techstack/javascript.svg", name: "JavaScript" },
+      { img: "/images/techstack/typescript.svg", name: "TypeScript" },
+      { img: "/images/techstack/go.svg", name: "Go" },
+      { img: "/images/techstack/dart.svg", name: "Dart" },
     ],
   },
   {
     title: "Frameworks",
     items: [
-      { img: "", name: "Spring boot" },
-      { img: "", name: "Angular" },
-      { img: "", name: "React" },
-      { img: "", name: "Nest js" },
-      { img: "", name: "Node js" },
-      { img: "", name: "Flutter" },
-      { img: "", name: "Astro" },
+      { img: "/images/techstack/springboot.svg", name: "Spring boot" },
+      { img: "/images/techstack/angular.svg", name: "Angular" },
+      { img: "/images/techstack/react.svg", name: "React" },
+      { img: "/images/techstack/nest-js.svg", name: "Nest js" },
+      { img: "/images/techstack/node-js.svg", name: "Node js" },
+      { img: "/images/techstack/flutter.svg", name: "Flutter" },
+      { img: "/images/techstack/astro.svg", name: "Astro" },
     ],
   },
   {
     title: "Message & cache",
     items: [
-      { img: "", name: "Rabbit MQ" },
-      { img: "", name: "Redis" },
+      { img: "/images/techstack/rabbitmq.svg", name: "Rabbit MQ" },
+      { img: "/images/techstack/redis.svg", name: "Redis" },
+    ],
+  },
+  {
+    title: "Database",
+    items: [
+      { img: "/images/techstack/mariadb.svg", name: "MariaDB" },
+      { img: "/images/techstack/postgresql.svg", name: "PostgreSQL" },
     ],
   },
   {
     title: "Devops & Tools",
     items: [
-      { img: "", name: "Docker" },
-      { img: "", name: "Jenkins" },
+      { img: "/images/techstack/docker.svg", name: "Docker" },
+      { img: "/images/techstack/jenkins.svg", name: "Jenkins" },
     ],
   },
 ];
